e06fc605a2fd34692ce40bb8b03b2a5b1f1744af
Aryan4512 <aryan.chaurasia.786786@gmail.com>
2026-04-07 15:30:46 +0530

    practical

---
+++ Javascript/jsfundamentals/app.js
@@ -0,0 +1,14 @@
+// var name="john";
+// var age=30;
+// var isemployed=true;
+// var age=20;
+// console.log(name);
+// console.log(age);
+// console.log(isemployed);
+
+var name="john";
+let age=30;
+var isemployed=true;
+console.log(name);
+console.log(age);
+console.log(isemployed);
